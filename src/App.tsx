ce13f23741027125724446b1a7d61c50263f0f6e
Matias Turra <matias.turra@gmail.com>
2023-11-05 17:46:10 -0300

    TI-2 Add base HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <aside>
+          <h1>Technical Interview</h1>
+          <p>
+            Check <code>README.md</code> for instructions
+          </p>
+          <form>
+            <label>
+              Add elements to your list
+              <input
+                name="item"
+                required
+                type="text"
+                placeholder="Introduce element"
+              />
+            </label>
+            <button>Add</button>
+          </form>
+        </aside>
+        <section>
+          <h2>Elements</h2>
+          <ul>
+            <li>Element 1</li>
+            <li>Element 2</li>
+            <li>Element 3</li>
+            <li>Element 4</li>
+          </ul>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
